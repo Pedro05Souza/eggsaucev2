6f509b1d5d9668e40b43a7a3545bba68351e2027
Pedro <pedroh.fsouzars@gmail.com>
2025-01-02 18:28:57 -0300

    refact: methods

--- repositories/players/player_repository.py
+++ repositories/players/player_repository.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from tortoise.transactions import in_transaction
 from entities import PlayerEntity
 from models import Player, BankPlayer
 from ..mappers import player_model_to_entity
@@ -18,12 +19,24 @@ class PlayerRepository():
     async def create_player(self, discord_id: int) -> PlayerEntity:
         player = await Player.create(discord_user_id=discord_id)
         await self.__create_bank_player(player)
-        
-        
         return await player_model_to_entity(player)
     
     async def __create_bank_player(self, player: Player) -> BankPlayer:
         return await BankPlayer.create(player=player)
+    
+    async def update_player(self, player: PlayerEntity) -> PlayerEntity:
+        async with in_transaction():
+            await Player.filter(id=player.player_id).update(
+                balance=player.balance,
+                role_values=player.roles,
+                last_salary_time=player.last_salary_time
+            )
+            
+            await BankPlayer.filter(player=player.player_id).update(
+                balance=player.bank_balance,
+                upgrade_level=player.upgrade_level
+            )
+            return player
         
         
         
