60d4e8f544ab7d9d4165b0a021a216cde8961eeb
Pedro <pedroh.fsouzars@gmail.com>
2025-01-02 18:28:43 -0300

    refact: thumbnail always being None

--- tools/discord_utils.py
+++ tools/discord_utils.py
@@ -3,11 +3,8 @@ from discord.ext.commands import Context
 from discord import Interaction, Color, Embed, Forbidden
 from .error_reasons import REASON_DM_FAILURE
 
-__all__ = [
-    "send_bot_embed",	
-    "embed_builder",
-    "send_user_dm"
-]
+__all__ = ["send_bot_embed", "embed_builder", "send_user_dm"]
+
 
 async def send_bot_embed(
     ctx: Union[Context, Interaction],
@@ -15,6 +12,7 @@ async def send_bot_embed(
     footer_text: Optional[str] = None,
     ephemeral: bool = False,
     is_dm: bool = False,
+    thumbnail_url: Optional[str] = None,
     **kwargs
 ) -> None:
     """This function is responsable for sending an embed for the user.
@@ -25,7 +23,9 @@ async def send_bot_embed(
         footer_text (Optional[str], optional): The text that will be displayed in the footer of the embed. Defaults to None.
         ephemeral (bool, optional): A boolean that checks if the message should be sent privately within the server. Defaults to False.
         is_dm (bool, optional): Checks if the message should be sent privately to the user. Defaults to False.
-        
+        embed_file (Optional[str], optional): The file that will be sent with the embed. Defaults to None.
+        thumbnail_url (Optional[str], optional): The URL of the thumbnail that will be displayed in the embed. Defaults to None.
+
     Raises:
         ValueError: If ephemeral and is_dm are both True or if ephemeral is True and the context is not an interaction.
     """
@@ -36,27 +36,31 @@ async def send_bot_embed(
 
     if ephemeral and not is_interaction:
         raise ValueError("Ephemeral can only be used with interactions")
-
+    
     if is_interaction:
         if not ctx.response.is_done():
             return await ctx.response.send_message(
                 embed=await embed_builder(
-                    color=color, footer_text=footer_text, **kwargs
+                    color=color, footer_text=footer_text, thumbnail_url=thumbnail_url, **kwargs
                 ),
                 ephemeral=ephemeral,
             )
 
         return await ctx.followup.send(
-            embed=await embed_builder(color=color, footer_text=footer_text, **kwargs),
+            embed=await embed_builder(color=color, footer_text=footer_text, thumbnail_url=thumbnail_url, **kwargs),
             ephemeral=ephemeral,
         )
 
     return await ctx.send(
-        embed=await embed_builder(color=color, footer_text=footer_text, **kwargs)
+        embed=await embed_builder(color=color, footer_text=footer_text, thumbnail_url=thumbnail_url, **kwargs),
     )
 
+
 async def embed_builder(
-    color: str, footer_text: Optional[str], **kwargs
+    color: str,
+    footer_text: Optional[str] = None,
+    thumbnail_url: Optional[str] = None,
+    **kwargs
 ) -> Embed:
     """This function is responsable for building the embed that will be sent to the user.
 
@@ -72,8 +76,12 @@ async def embed_builder(
 
     if footer_text:
         embed.set_footer(text=footer_text)
+        
+    if thumbnail_url:
+        embed.set_thumbnail(url=thumbnail_url)
 
     return embed
+
 
 async def send_user_dm(ctx: Union[Context, Interaction], embed: Embed) -> None:
     """This function is responsable for sending a message to the user's DM.
