a4846417254c7406ca471eefe08cc5e3465ade27
Pedro <pedroh.fsouzars@gmail.com>
2025-01-03 16:03:34 -0300

    feat: steal command

--- controllers/player_controller.py
+++ controllers/player_controller.py
@@ -1,11 +1,12 @@
+from random import Random
 from discord.ext.commands import Cog, hybrid_command, Bot, Context
 from discord import Member
-from tools import database_user
 from repositories import PlayerRepository
-from usecases import BalanceUsecase, DonateUsecase
+from tools import database_user
+from usecases import BalanceUsecase, DonateUsecase, StealUsecase
 
 class PlayerController(Cog):
-    player_repo: PlayerRepository = PlayerRepository()
+    player_repo = PlayerRepository()
     
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
@@ -13,14 +14,21 @@ class PlayerController(Cog):
     @hybrid_command(name="balance", aliases=["bal", "points", "p"])
     async def balance(self, ctx: Context, member: Member = None) -> None:  
         member = member if member else ctx.author
-        player_usecase = BalanceUsecase(ctx, member, self.player_repo)
-        await player_usecase.get_player_balance()
+        balance_usecase = BalanceUsecase(ctx, member, self.player_repo)
+        await balance_usecase.get_player_balance()
         
     @hybrid_command(name="donate", aliases=["give"])
     @database_user(player_repo)
     async def donate(self, ctx: Context, amount: int, recipient: Member) -> None:
-        player_usecase = DonateUsecase(ctx, ctx.player_entity, amount, recipient, self.player_repo)
-        await player_usecase.donate()
+        donate_usecase = DonateUsecase(ctx, ctx.player_entity, amount, recipient, self.player_repo)
+        await donate_usecase.donate()
+        
+    @hybrid_command(name="steal", aliases=["rob"])
+    @database_user(player_repo)
+    async def steal(self, ctx: Context, target: Member) -> None:
+        random = Random()
+        steal_usecase = StealUsecase(ctx, ctx.player_entity, target, self.player_repo, random)
+        await steal_usecase.steal()
         
 async def setup(bot: Bot) -> None:
     await bot.add_cog(PlayerController(bot))
---
+++ usecases/steal_usecase.py
@@ -0,0 +1,66 @@
+from random import Random
+from discord.ext.commands import Context
+from discord import Member
+from tortoise.transactions import in_transaction
+from repositories import PlayerRepository
+from entities import PlayerEntity
+from tools import send_failed_embed, send_bot_embed
+from tools.constants import (
+    MAX_PERCETANGE_TO_STEAL,
+    STEAL_FAILURE_CHANCE,
+    REASON_INVALID_USER,
+    REASON_CANT_ACTION_SELF,
+    REASON_STEAL_NO_MONEY,
+)
+
+
+class StealUsecase:
+
+    def __init__(
+        self,
+        ctx: Context,
+        stealer: PlayerEntity,
+        target: Member,
+        player_repo: PlayerRepository,
+        random: Random,
+    ) -> None:
+        self.ctx = ctx
+        self.stealer = stealer
+        self.target = target
+        self.player_repo = player_repo
+        self.random = random
+
+    async def steal(self) -> None:
+        if self.target.id == self.stealer.id:
+            return await send_failed_embed(self.ctx, REASON_CANT_ACTION_SELF)
+
+        target_entity = await self.player_repo.get_player_by_discord_id(self.target.id)
+
+        if not target_entity or target_entity.id == self.stealer.id:
+            return await send_failed_embed(self.ctx, REASON_INVALID_USER)
+
+        if target_entity.balance == 0:
+            return await send_failed_embed(self.ctx, REASON_STEAL_NO_MONEY)
+        
+        max_steal_amount = int(target_entity.balance * MAX_PERCETANGE_TO_STEAL)
+        
+        random_number = self.random.random()
+        
+        if random_number < STEAL_FAILURE_CHANCE:
+            return await send_failed_embed(self.ctx, "Your attempt to steal failed.")
+        
+        stolen_amount = self.random.randint(1, max_steal_amount)
+        
+        self.stealer.balance += stolen_amount
+        target_entity.balance -= stolen_amount
+        
+        async with in_transaction():
+            await self.player_repo.update_player(self.stealer)
+            await self.player_repo.update_player(target_entity)
+            return await send_bot_embed(
+                ctx=self.ctx,
+                title="✅ Steal successful",
+                description=f"You stole **{stolen_amount}** eggbux from {self.target.mention}",
+            )
+        
+        
