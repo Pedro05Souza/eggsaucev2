bb75c6b06fb84624bc212d93048fc8d8def9b637
Pedro <pedroh.fsouzars@gmail.com>
2025-01-05 00:00:14 -0300

    refact: transform this into an object

--- usecases/gain_points_usecase.py
+++ usecases/gain_points_usecase.py
@@ -1,47 +1,71 @@
-from typing import Union
+from typing import Union, Optional
+from discord import VoiceState
 from repositories import PlayerRepository
 from tools import PointsService
 from entities import PlayerEntity
 
-class GainPointsUsecase:
 
-    @staticmethod
-    async def calculate_points(
+class GainPointsUsecase:
+    
+    async def calculate_points_message(
+        self,
         player_entity_or_discord_id: Union[PlayerEntity, int],
         player_repo: PlayerRepository,
         points_service: PointsService,
-        calculation_type: int
     ) -> None:
-        """This method calculates the points for a player and updates their balance. This is used by message and voice events.
-
-        Args:
-            player_entity_or_discord_id (Union[PlayerEntity, int]): The player entity or discord id. # In certain contexts we can't pass the entity, so we pass the discord id instead (e.g listeners)
-            player_repo (PlayerRepository): The player repository
-            points_service (PointsService): The points service
-            calculation_type (int): The calculation flag (0 for message, 1 for voice)
-
-        Raises:
-            ValueError: If the calculation flag is invalid
-        """
-        if isinstance(player_entity_or_discord_id, int):
-            player_entity = await player_repo.get_player_by_discord_id(player_entity_or_discord_id)
-        else:
-            player_entity = player_entity_or_discord_id
+        player_entity = await self.__get_player_entity_or_none(
+            player_entity_or_discord_id, player_repo
+        )
         
-        if calculation_type == 0:
-            calculated_points = await points_service.calculate_points_message(
-                player_entity.discord_user_id
-            )
-        elif calculation_type == 1:
-            calculated_points = await points_service.calculate_points_voice(
-                player_entity.discord_user_id
-            )
-        else:
-            raise ValueError("Invalid calculation type")
+        if not player_entity:
+            return
+
+        calculated_points = await points_service.calculate_points_message(
+            player_entity.discord_user_id
+        )
 
         if calculated_points == 0:
             return
-        
+
         player_entity.balance += calculated_points
 
         await player_repo.update_player(player_entity)
+        
+        
+    async def calculate_points_voice(
+        self,
+        player_entity_or_discord_id: Union[PlayerEntity, int],
+        player_repo: PlayerRepository,
+        points_service: PointsService,
+        before: VoiceState,
+        after: VoiceState,
+    ) -> None:
+        player_entity = await self.__get_player_entity_or_none(
+            player_entity_or_discord_id, player_repo
+        )
+        
+        if not player_entity:
+            return
+
+        calculated_points = await points_service.calculate_points_voice(
+            player_entity.discord_user_id, before, after
+        )
+
+        if calculated_points == 0:
+            return
+
+        player_entity.balance += calculated_points
+
+        await player_repo.update_player(player_entity)
+        
+    async def __get_player_entity_or_none(
+        self, player_entity_or_discord_id: Union[PlayerEntity, int], player_repo: PlayerRepository
+    ) -> Optional[PlayerEntity]:
+        if isinstance(player_entity_or_discord_id, int):
+            player_entity = await player_repo.get_player_by_discord_id(
+                player_entity_or_discord_id
+            )
+        else:
+            player_entity = player_entity_or_discord_id
+
+        return player_entity
