f12af2f29d770c7efc882de008af270a83da7803
Pedro <pedroh.fsouzars@gmail.com>
2025-01-04 23:59:16 -0300

    refact: extra white spaces

--- controllers/developer_controller.py
+++ controllers/developer_controller.py
@@ -2,16 +2,18 @@ from discord.ext.commands import Cog, Bot, command, Context
 from usecases import ReloadCogsUsecase
 from tools import dev_only
 
+
 class DeveloperController(Cog):
-    
+
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
-        
+
     @command(name="reload", aliases=["r"])
     @dev_only()
     async def reload(self, _: Context) -> None:
         reload_usecase = ReloadCogsUsecase(self.bot)
         await reload_usecase.reload_cogs()
-    
+
+
 async def setup(bot: Bot) -> None:
     await bot.add_cog(DeveloperController(bot))
