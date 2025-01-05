39e48c2bfe886c58cd1a92eb2e67aeb39ffe8f23
Pedro <pedroh.fsouzars@gmail.com>
2025-01-04 23:59:38 -0300

    refact: the way dependancies injection works

--- controllers/player_controller.py
+++ controllers/player_controller.py
@@ -1,25 +1,29 @@
 from random import Random
+from datetime import datetime
 from discord.ext.commands import Cog, hybrid_command, Bot, Context
-from discord import Member, Message
+from discord import Member, Message, VoiceState
 from repositories import PlayerRepository
-from tools import database_user, PointsService
+from tools import database_user, PointsService, CacheService
 from usecases import BalanceUsecase, DonateUsecase, StealUsecase, GainPointsUsecase
 
-class PlayerController(Cog):
-    player_repo = PlayerRepository()
 
-    def __init__(self, bot: Bot) -> None:
+class PlayerController(Cog):
+
+    def __init__(
+        self, bot: Bot, player_repo: PlayerRepository, points_service: PointsService
+    ) -> None:
         self.bot = bot
-        self.points_service = PointsService()
+        self.player_repo = player_repo
+        self.points_service = points_service
 
     @hybrid_command(name="balance", aliases=["bal", "points", "p"])
     async def balance(self, ctx: Context, member: Member = None) -> None:
         member = member if member else ctx.author
         balance_usecase = BalanceUsecase(ctx, member, self.player_repo)
         await balance_usecase.get_player_balance()
-
+        
     @hybrid_command(name="donate", aliases=["give"])
-    @database_user(player_repo)
+    @database_user()
     async def donate(self, ctx: Context, amount: int, recipient: Member) -> None:
         donate_usecase = DonateUsecase(
             ctx, ctx.player_entity, amount, recipient, self.player_repo
@@ -27,7 +31,7 @@ class PlayerController(Cog):
         await donate_usecase.donate()
 
     @hybrid_command(name="steal", aliases=["rob"])
-    @database_user(player_repo)
+    @database_user()
     async def steal(self, ctx: Context, target: Member) -> None:
         random = Random()
         steal_usecase = StealUsecase(
@@ -39,10 +43,24 @@ class PlayerController(Cog):
     async def on_message(self, message: Message) -> None:
         if message.author.bot:
             return
-        
-        await GainPointsUsecase.calculate_points(
-            message.author.id, self.player_repo, self.points_service, 0
+
+        g = GainPointsUsecase()
+
+        await g.calculate_points_message(
+            message.author.id, self.player_repo, self.points_service
+        )
+
+    @Cog.listener()
+    async def on_voice_state_update(
+        self, member: Member, before: VoiceState, after: VoiceState
+    ) -> None:
+        g = GainPointsUsecase()
+
+        await g.calculate_points_voice(
+            member.id, self.player_repo, self.points_service, before, after
         )
 
 async def setup(bot: Bot) -> None:
-    await bot.add_cog(PlayerController(bot))
+    player_repo = PlayerRepository()
+    points_service = PointsService(CacheService[int, datetime])
+    await bot.add_cog(PlayerController(bot, player_repo, points_service))
--- tools/decorators.py
+++ tools/decorators.py
@@ -3,32 +3,38 @@ from discord.ext import commands
 from repositories import PlayerRepository
 from tools.constants import get_env_var
 
-__all__ = ['dev_only', 'database_user']
+__all__ = ["dev_only", "database_user"]
+
 
 def dev_only():
     async def predicate(ctx: Context) -> bool:
         dev_ids = get_env_var("LIST_DEVELOPER_IDS")
-        
+
         if ctx.author.id not in dev_ids:
             return False
-        
+
         return True
 
     return commands.check(predicate)
 
-def database_user(player_repo: PlayerRepository):
+
+def database_user():
     """Fetches the player entity from the database and attaches it to the context.
 
     Args:
         player_repo (PlayerRepository): The repository that will be used to fetch the player entity.
     """
+
     async def predicate(ctx: Context) -> bool:
+        player_repo: PlayerRepository = ctx.cog.player_repo
+
         player_entity = await player_repo.get_player_by_discord_id(ctx.author.id)
-           
+
         if not player_entity:
             player_entity = await player_repo.create_player(ctx.author.id)
-            
+
         ctx.player_entity = player_entity
 
         return True
+
     return check(predicate)
