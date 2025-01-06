bb4895f14599a8793a4295bd9431332ffc4e86b7
Pedro <pedroh.fsouzars@gmail.com>
2025-01-06 01:32:05 -0300

    feat: sync command

--- controllers/developer_controller.py
+++ controllers/developer_controller.py
@@ -1,18 +1,26 @@
 from discord.ext.commands import Cog, Bot, command, Context
 from usecases import ReloadCogsUsecase
-from tools import dev_only
+from tools import dev_only, get_logger
 
 
 class DeveloperController(Cog):
 
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
+        self.logger = get_logger(__name__)
 
     @command(name="reload", aliases=["r"])
     @dev_only()
     async def reload(self, _: Context) -> None:
         reload_usecase = ReloadCogsUsecase(self.bot)
         await reload_usecase.reload_cogs()
+        self.logger.info("Reloaded cogs.")
+        
+    @command(name="sync")
+    @dev_only()
+    async def sync(self, _: Context) -> None:
+        await self.bot.tree.sync()
+        self.logger.info("Synced tree.")
 
 async def setup(bot: Bot) -> None:
     await bot.add_cog(DeveloperController(bot))
