ce3d3c1f1a07146978b2acc4ac5f474dbadf3aa8
Pedro <pedroh.fsouzars@gmail.com>
2025-01-07 12:39:00 -0300

    refact: change function parameters

--- usecases/balance_usecase.py
+++ usecases/balance_usecase.py
@@ -2,7 +2,7 @@ from datetime import timedelta
 from discord.ext.commands import Context
 from discord import Member
 from discord.utils import format_dt
-from tools import send_bot_embed, PlayerCacheService
+from tools import send_bot_embed, PlayerCacheService, send_failed_embed
 from tools.constants import SECONDS_TO_SALARY_DROP, REASON_INVALID_USER
 
 __all__ = ["BalanceUsecase"]
@@ -19,10 +19,9 @@ class BalanceUsecase:
         player_entity = await self.player_cache.get_or_add_player_entity(self.discord_member.id)
 
         if not player_entity:
-            return await send_bot_embed(
-                ctx=self.ctx,
-                title="❌ Balance failed",
-                description=REASON_INVALID_USER,
+            return await send_failed_embed(
+                self.ctx,
+                REASON_INVALID_USER,
             )
 
         description = (
