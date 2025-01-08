396bff62d4a88de92daa9fe2be5d21ce2e4fb3e9
Pedro <pedroh.fsouzars@gmail.com>
2025-01-07 21:01:43 -0300

    refact: move ChickenRarityEnum to chicken_config and remove model_enums

---
+++ tools/constants/shared_enums.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class PlayerTitles(str, Enum):
+    DEFAULT = "default"
+    NOOB = "noob"
+    BEGINNER = "beginner"
+    INTERMEDIATE = "intermediate"
+    ADVANCED = "advanced"
+    EXPERT = "expert"
+    MASTER = "master"
