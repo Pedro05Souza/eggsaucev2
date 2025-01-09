6920f6c0ee94f47192e8c7abeb3e4dc2620a0bf5
Leonardo <leonardoms.2010@hotmail.com>
2025-01-09 16:46:34 -0300

    refact: deposit usercase adjusts

--- usecases/deposit_usecase.py
+++ usecases/deposit_usecase.py
@@ -5,34 +5,34 @@ from tools import (
     send_bot_embed,
     send_failed_embed,
 )
-from tools.constants import REASON_INVALID_AMOUNT,  REASON_INSUFFICIENT_BALANCE, REASON_INVALID_USER
+from tools.constants import REASON_INVALID_AMOUNT, REASON_INSUFFICIENT_BALANCE, REASON_INVALID_USER
 
 
 __all__ = ["DepositUsecase"]
 
+
 class DepositUsecase:
-    
+
     def __init__(self, ctx: Context, player_entity: PlayerEntity, player_cache: PlayerCacheService) -> None:
         self.ctx = ctx
         self.player_entity = player_entity
         self.player_cache = player_cache
 
     async def deposit(self, amount: int) -> None:
-        balance = self.player_entity.balance
-        bank_balance = self.player_entity.bank_balance
-        
-        if amount < 0:
+        if amount <= 0:
             return await send_failed_embed(self.ctx, REASON_INVALID_AMOUNT)
-        
-        if amount > balance:
+
+        if amount > self.player_entity.balance:
             return await send_failed_embed(self.ctx, REASON_INSUFFICIENT_BALANCE)
-        
-        self.player_entity.bank_balance  = bank_balance + amount
-        self.player_entity.balance = balance - amount
-        
-        # self.player_entity.bank_capacity
+
+        if amount > self.player_entity.bank_capacity:
+            return await send_failed_embed(self.ctx, "You can't do this")
+
+        self.player_entity.bank_balance += amount
+        self.player_entity.balance -= amount
+        await self.player_cache.player_synchronizer(self.player_entity)
         return await send_bot_embed(
             ctx=self.ctx,
-            title=f"✅ Deposit was sucessfull",
-            description=f"You deposit {amount} successfully in your bank",
+            title="✅ Deposit was sucessfull",
+            description=f"You deposited **{amount}** eggbux successfully in your bank account.",
         )
