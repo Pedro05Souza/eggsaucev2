be418bf142ff131c0362c1fa0575f6eeae4177bc
Pedro <pedroh.fsouzars@gmail.com>
2025-01-10 18:25:32 -0300

    refact:

--- repositories/mappers/player_to_entity.py
+++ repositories/mappers/player_to_entity.py
@@ -1,18 +1,18 @@
 from entities import PlayerEntity
-from models import Player
+from models import Player, BankPlayer
 from tools.constants import AMOUNT_PER_BANK_UPGRADE
 
 __all__ = ["player_model_to_entity"]
 
 def player_model_to_entity(player: Player) -> PlayerEntity:
-
+    bank_player: BankPlayer = player.bank_player # type: ignore
     return PlayerEntity(
         id=str(player.id),
         discord_user_id=player.discord_user_id,
         balance=player.balance,
         last_bought_title=player.last_bought_title.value if player.last_bought_title else None,
         next_salary_time=player.next_salary_time,
-        bank_balance=player.bank_player.balance, # type: ignore
-        bank_capacity=player.bank_player.upgrade_level * AMOUNT_PER_BANK_UPGRADE, # type: ignore
-        upgrade_level=player.bank_player.upgrade_level, # type: ignore
+        bank_balance=bank_player.balance,
+        bank_capacity=bank_player.upgrade_level * AMOUNT_PER_BANK_UPGRADE,
+        upgrade_level=bank_player.upgrade_level,
     )
