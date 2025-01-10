c9b00d11c49d40039dc533f992833030419fba91
Leonardo <leonardoms.2010@hotmail.com>
2025-01-10 19:38:21 -0300

    refact: withdraw_usecase changes

--- usecases/withdraw_usecase.py
+++ usecases/withdraw_usecase.py
@@ -13,25 +13,28 @@ __all__ = ["WithdrawUsecase"]
 
 class WithdrawUsecase:
 
-    def __init__(self, ctx: Context, player_entity: PlayerEntity, player_cache: PlayerCacheService) -> None:
+    def __init__(
+        self, ctx: Context, player_entity: PlayerEntity, player_cache: PlayerCacheService, amount: int
+    ) -> None:
         self.ctx = ctx
         self.player_entity = player_entity
         self.player_cache = player_cache
+        self.amount = amount
 
-    async def withdraw(self, amount: int) -> None:
+    async def withdraw(self) -> None:
 
-        if amount < 0:
+        if self.amount < 0:
             return await send_failed_embed(self.ctx, REASON_INVALID_AMOUNT)
-        if amount > self.player_entity.bank_balance:
+        if self.amount > self.player_entity.bank_balance:
             return await send_failed_embed(self.ctx, REASON_INSUFFICIENT_BALANCE)
-        if amount > self.player_entity.bank_capacity:
+        if self.amount > self.player_entity.bank_capacity:
             return await send_failed_embed(self.ctx, "You can't do this")
 
-        self.player_entity.bank_balance -= amount
-        self.player_entity.balance += amount
+        self.player_entity.bank_balance -= self.amount
+        self.player_entity.balance += self.amount
         await self.player_cache.player_synchronizer(self.player_entity)
         return await send_bot_embed(
             ctx=self.ctx,
             title="✅Withdrawal was successful",
-            description=f"You withdraw {amount} successfully from the bank",
+            description=f"You withdraw {self.amount} successfully from the bank",
         )
