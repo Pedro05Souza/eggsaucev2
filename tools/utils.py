ff4504fea06f87547ddd0cffa6e6d3f5f9c5199c
Pedro <pedroh.fsouzars@gmail.com>
2025-01-10 21:39:12 -0300

    feat: new shared function

--- tools/utils.py
+++ tools/utils.py
@@ -1,11 +1,12 @@
 from logging import Logger, config, getLogger
 from entities import PlayerEntity
-from .constants import LOGGING_CONFIG
+from .constants import LOGGING_CONFIG, get_titles_salaries
 
 __all__ = [
     "get_logger",
     "get_balance_diff",
     "deduct_from_balance_and_bank",
+    'get_salary_from_title'
 ]
 
 
@@ -13,9 +14,11 @@ def get_logger(name: str) -> Logger:
     config.dictConfig(LOGGING_CONFIG)
     return getLogger(name)
 
+
 def get_balance_diff(player_entity: PlayerEntity, price: int) -> int:
     total_balance = player_entity.balance + player_entity.bank_balance
     return total_balance - price
+
 
 def deduct_from_balance_and_bank(player_entity: PlayerEntity, price: int) -> None:
     if player_entity.balance >= price:
@@ -24,3 +27,8 @@ def deduct_from_balance_and_bank(player_entity: PlayerEntity, price: int) -> Non
         price -= player_entity.balance
         player_entity.balance = 0
         player_entity.bank_balance -= price
+
+
+def get_salary_from_title(title: str) -> int:
+    titles_prices = get_titles_salaries()
+    return titles_prices.get(title, 0)
