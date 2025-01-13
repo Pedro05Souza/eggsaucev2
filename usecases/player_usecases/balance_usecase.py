b7981515484695b194eaa77ad302df77ae16092e
Pedro <pedroh.fsouzars@gmail.com>
2025-01-12 22:18:05 -0300

    feat: calculate away time in balance command

--- usecases/player_usecases/balance_usecase.py
+++ usecases/player_usecases/balance_usecase.py
@@ -1,7 +1,13 @@
 from discord.ext.commands import Context
 from discord import Member
 from discord.utils import format_dt
-from tools import send_bot_embed, PlayerCacheService, send_failed_embed
+from tools import (
+    send_bot_embed,
+    PlayerCacheService,
+    send_failed_embed,
+    calculate_away_time_earnings,
+    AwayTimeEarningsService,
+)
 from tools.constants import REASON_INVALID_USER
 
 __all__ = ["BalanceUsecase"]
@@ -9,22 +15,27 @@ __all__ = ["BalanceUsecase"]
 
 class BalanceUsecase:
 
-    def __init__(self, ctx: Context, discord_member: Member | None, player_cache: PlayerCacheService) -> None:
+    def __init__(
+        self,
+        ctx: Context,
+        discord_member: Member | None,
+        player_cache: PlayerCacheService,
+        away_time_earnings_service: AwayTimeEarningsService,
+    ) -> None:
         self._ctx = ctx
         self._discord_member = discord_member
         self._player_cache = player_cache
+        self._away_time_earnings_service = away_time_earnings_service
 
-    async def get_player_balance(self) -> None:
+    async def balance(self) -> None:
 
         avatar_to_send = None
 
         if self._discord_member:
-            player_entity = await self._player_cache.get_or_fetch_player_entity(
-                self._discord_member.id, is_readonly=True
-            )
+            player_entity = await self._player_cache.get_or_fetch_player_entity(self._discord_member.id)
             avatar_to_send = self._discord_member.display_avatar.url
         else:
-            player_entity = await self._player_cache.get_or_fetch_player_entity(self._ctx.author.id, is_readonly=True)
+            player_entity = await self._player_cache.get_or_fetch_player_entity(self._ctx.author.id)
             avatar_to_send = self._ctx.author.display_avatar.url
 
         if not player_entity:
@@ -34,6 +45,8 @@ class BalanceUsecase:
                     REASON_INVALID_USER,
                 )
             player_entity = await self._player_cache.create_player(self._ctx.author.id)
+
+        await calculate_away_time_earnings(player_entity, self._player_cache, self._away_time_earnings_service)
 
         description = (
             f"💸 Wallet: **{player_entity.balance}**"
