8c6d5139b792046163bb0a925eace93e41d8db08
Pedro <pedroh.fsouzars@gmail.com>
2025-01-13 23:27:00 -0300

    refact: remove SingletonMeta from DatabaseStarterService class

--- tools/services/database_starter_service.py
+++ tools/services/database_starter_service.py
@@ -1,10 +1,9 @@
 from tortoise import Tortoise, run_async
 from ..constants import TORTOISE_ORM
-from ._singleton_meta import SingletonMeta
 
 __all__ = ['DatabaseStarterService']
 
-class DatabaseStarterService(metaclass=SingletonMeta):
+class DatabaseStarterService():
 
     def __init__(self) -> None:
         run_async(self.__setup())
