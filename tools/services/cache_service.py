23a1d642076c7e43466f93ebf9af92e97247d8c2
Pedro <pedroh.fsouzars@gmail.com>
2025-01-13 23:26:19 -0300

    feat: add logging and rollback mechanism

--- tools/services/cache_service.py
+++ tools/services/cache_service.py
@@ -1,5 +1,7 @@
-from typing import Generic, TypeVar, Optional
+from typing import Generic, TypeVar, Optional, Any, AsyncGenerator
+from contextlib import asynccontextmanager
 from cachetools import TTLCache
+from tools.utils import get_logger
 
 __all__ = ["CacheService"]
 
@@ -11,6 +13,7 @@ class CacheService(Generic[K, V]):
 
     def __init__(self, max_size: int = 100, expiration_time: int = 3600) -> None:
         self._cache: TTLCache[K, V] = TTLCache(maxsize=int(max_size), ttl=int(expiration_time))
+        self.logger = get_logger(__name__)
 
     def add_item(self, key: K, value: V) -> bool:
         if key not in self._cache:
@@ -28,3 +31,21 @@ class CacheService(Generic[K, V]):
         if key in self._cache:
             return self._cache[key]
         return None
+
+    @asynccontextmanager
+    async def _revert_if_exception(self, entity: V, previous_state: dict[str, Any]) -> AsyncGenerator[None, Any]:
+        """Reverts the entity to its previous state if an exception occurs.
+
+        Args:
+            entity (type(entity)): The entity to revert.
+            previous_state (dict[str, Any]): The previous state of the entity.
+
+        Returns:
+            AsyncGenerator[None, Any]: An async generator.
+        """
+        try:
+            yield
+        except Exception as e:
+            for key, value in previous_state.items():
+                setattr(entity, key, value)
+            self.logger.exception("Failed to update cache: %s", e)
