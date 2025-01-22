ebdbe92e9b36e3e37e94c93d6ceabd3b4c0ee963
Pedro <pedroh.fsouzars@gmail.com>
2025-01-22 19:52:45 -0300

    feat: farm repository

--- repositories/farm_repository.py
+++ repositories/farm_repository.py
@@ -1,6 +1,6 @@
 from typing import Optional
 from entities import FarmEntity
-from models import Farm
+from models import Farm, Player, Chicken
 from .mappers import farm_model_to_entity
 
 __all__ = ["FarmRepository"]
@@ -9,7 +9,8 @@ __all__ = ["FarmRepository"]
 class FarmRepository:
 
     async def get_farm_by_discord_user_id(self, discord_user_id: int) -> Optional[FarmEntity]:
-        farm = await Farm.get_or_none(player_id=discord_user_id).select_related("player").select_related("chickens")
+        player = await Player.get(discord_user_id=discord_user_id)
+        farm = await Farm.get_or_none(player_id=player.id).select_related("player").prefetch_related("chickens")
 
         if not farm:
             return None
@@ -17,6 +18,26 @@ class FarmRepository:
         return await farm_model_to_entity(farm)
 
     async def create_farm(self, discord_user_id: int) -> FarmEntity:
-        farm = await Farm.create(player_id=discord_user_id)
-        farm = await Farm.get(id=farm.id).select_related("player").select_related("chickens")
+        player = await Player.get(discord_user_id=discord_user_id)
+        farm = await Farm.create(player_id=player.id)
+        farm = await Farm.get(id=farm.id).select_related("player")
         return await farm_model_to_entity(farm)
+
+    async def update_farm(self, farm_entity: FarmEntity) -> FarmEntity:
+        await Farm.filter(id=farm_entity.id).update(
+            farm_title=farm_entity.farm_title,
+            farmer=farm_entity.farmer,
+            next_drop_time=farm_entity.next_drop_time,
+            remaining_rolls=farm_entity.remaining_rolls,
+            next_chicken_roll_time=farm_entity.next_chicken_roll_time,
+        )
+        return farm_entity
+
+    async def bulk_upsert_farm_chicken(self, chickens: list[Chicken], is_updated: bool) -> None:
+        if not is_updated:
+            await Chicken.bulk_create(chickens)
+        else:
+            await Chicken.bulk_update(
+                chickens,
+                fields=["farm_id", "name", "eggs_generated", "quality", "rarity", "location_status", "happiness"],
+            )
