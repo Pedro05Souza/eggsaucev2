bdb72848fa4b100aa4a0625ab4ca3d909589b9c3
Pedro <pedroh.fsouzars@gmail.com>
2025-01-24 17:23:13 -0300

    test: refactor player cache service tests to use async fixtures and improve database initialization

--- tests/test_player_cache_service.py
+++ tests/test_player_cache_service.py
@@ -1,5 +1,7 @@
 import pytest
+import pytest_asyncio
 from pytest_mock import MockerFixture
+from tortoise import Tortoise
 from tools import PlayerCacheService
 from tools.services._proxy_objects import MutableProxy
 from tools.constants import NoUpdateRequiredException, NotInCacheException
@@ -7,14 +9,21 @@ from entities import PlayerEntity
 from repositories import PlayerRepositoryProtocol
 
 
-@pytest.fixture
-def mock_player_repository(mocker: MockerFixture):
+@pytest_asyncio.fixture(scope="session", autouse=True)
+async def initialize_db():
+    await Tortoise.init(db_url="sqlite://:memory:", modules={"models": ["models"]})
+    yield
+    await Tortoise.close_connections()
+
+
+@pytest.fixture(name="player_repository")
+def player_repository(mocker: MockerFixture):
     return mocker.Mock(spec=PlayerRepositoryProtocol)
 
 
 @pytest.fixture
-def player_cache_service(mock_player_repository):
-    return PlayerCacheService(track_evict=True, player_repository=mock_player_repository)
+def player_cache_service(player_repository):
+    return PlayerCacheService(track_evict=True, player_repository=player_repository)
 
 
 @pytest.fixture
@@ -47,53 +56,49 @@ def player_entity():
 
 
 @pytest.mark.asyncio
-async def test_get_or_fetch_player_entity_returns_none(
-    player_cache_service: PlayerCacheService, mock_player_repository
-):
+async def test_get_or_fetch_player_entity_returns_none(player_cache_service: PlayerCacheService, player_repository):
     discord_user_id = 1
-    mock_player_repository.get_player_by_discord_id.return_value = None
+    player_repository.get_player_by_discord_id.return_value = None
 
     result = await player_cache_service.get_or_fetch_player_entity(discord_user_id)
 
     assert result is None
-    mock_player_repository.get_player_by_discord_id.assert_called_once_with(discord_user_id)
+    player_repository.get_player_by_discord_id.assert_called_once_with(discord_user_id)
 
 
 @pytest.mark.asyncio
 async def test_get_or_fetch_player_entity_returns_player_entity(
-    player_cache_service, mock_player_repository, player_entity_proxy, player_entity
+    player_cache_service, player_repository, player_entity_proxy, player_entity
 ):
     discord_user_id = 1
-    mock_player_repository.get_player_by_discord_id.return_value = player_entity
+    player_repository.get_player_by_discord_id.return_value = player_entity
 
     result = await player_cache_service.get_or_fetch_player_entity(discord_user_id)
 
     assert player_entity_proxy == result
-    mock_player_repository.get_player_by_discord_id.assert_called_once_with(discord_user_id)
+    player_repository.get_player_by_discord_id.assert_called_once_with(discord_user_id)
 
 
 @pytest.mark.asyncio
-async def test_update_player_entity(player_cache_service, mock_player_repository, player_entity_proxy, player_entity):
+async def test_update_player_entity(player_cache_service, player_repository, player_entity_proxy, player_entity):
     player_cache_service.add_item(player_entity.discord_user_id, player_entity)
 
     player_entity_proxy.balance = 200
 
     await player_cache_service.synchronizer(player_entity_proxy)
-    mock_player_repository.update_player.assert_awaited_once_with(player_entity)
+    player_repository.update_player.assert_awaited_once_with(player_entity)
 
     assert player_entity.balance == 200
 
 
 @pytest.mark.asyncio
-async def test_update_player_bank_entity(
-    player_cache_service, mock_player_repository, player_entity_proxy, player_entity
-):
+async def test_update_player_bank_entity(player_cache_service, player_repository, player_entity_proxy, player_entity):
     player_cache_service.add_item(player_entity.discord_user_id, player_entity)
 
     player_entity_proxy.bank_balance = 200
 
     await player_cache_service.synchronizer(player_entity_proxy)
-    mock_player_repository.update_player_bank.assert_awaited_once_with(player_entity)
+    player_repository.update_player_bank.assert_awaited_once_with(player_entity)
 
     assert player_entity.bank_balance == 200
 
@@ -115,12 +120,12 @@ async def test_not_in_cache_update_player_entity(player_cache_service, player_en
 
 @pytest.mark.asyncio
 async def test_rollback_update_player_entity(
-    player_cache_service, mock_player_repository, player_entity_proxy, player_entity
+    player_cache_service, player_repository, player_entity_proxy, player_entity
 ):
     player_cache_service.add_item(player_entity.discord_user_id, player_entity)
 
     player_entity_proxy.balance = 200
-    mock_player_repository.update_player.side_effect = Exception()
+    player_repository.update_player.side_effect = Exception()
     await player_cache_service.synchronizer(player_entity_proxy)
 
     assert player_entity.balance == 100
@@ -128,12 +133,35 @@ async def test_rollback_update_player_entity(
 
 @pytest.mark.asyncio
 async def test_rollback_update_player_bank_entity(
-    player_cache_service, mock_player_repository, player_entity_proxy, player_entity
+    player_cache_service, player_repository, player_entity_proxy, player_entity
 ):
     player_cache_service.add_item(player_entity.discord_user_id, player_entity)
 
-    player_entity_proxy.bank_balance = 200
-    mock_player_repository.update_player_bank.side_effect = Exception()
+    player_entity_proxy.bank_balance = 100
+    player_entity_proxy.balance = 0
+    player_repository.update_player_bank.side_effect = Exception()
     await player_cache_service.synchronizer(player_entity_proxy)
 
     assert player_entity.bank_balance == 0
+    assert player_entity.balance == 100
+
+
+@pytest.mark.asyncio
+async def test_create_player_entity(player_cache_service, player_repository):
+    discord_user_id = 1
+    player_repository.create_player.return_value = PlayerEntity(
+        id="asdsad",
+        discord_user_id=1,
+        balance=100,
+        last_bought_title=None,
+        next_salary_time=None,
+        bank_balance=0,
+        bank_capacity=0,
+        upgrade_level=0,
+    )
+
+    result = await player_cache_service.create_player(discord_user_id)
+
+    assert result.discord_user_id == discord_user_id
+    player_repository.create_player.assert_awaited_once_with(discord_user_id)
+    assert player_cache_service.get_item(discord_user_id) == result
