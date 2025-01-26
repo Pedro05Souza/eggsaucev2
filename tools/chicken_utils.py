b28ea7661accaff5886a8082a6f3cc1cc3e5babb
Pedro <pedroh.fsouzars@gmail.com>
2025-01-26 17:59:57 -0300

    refact: remove print statement

--- tools/chicken_utils.py
+++ tools/chicken_utils.py
@@ -5,5 +5,4 @@ __all__ = ["get_quality_text"]
 
 def get_quality_text(chicken_quality: float) -> str:
     quality = round((chicken_quality * 100) / 10) * 10
-    print(quality)
     return chicken_quality_rates[quality]
