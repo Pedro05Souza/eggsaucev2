5c0b976c63018f1c5cb34af4c4f849056773560f
Pedro <pedroh.fsouzars@gmail.com>
2025-01-26 15:22:34 -0300

    feat: implement base cache classes and update LRU/TTL cache services

--- tools/services/cache/lru_cache_service.py
+++ tools/services/cache/lru_cache_service.py
@@ -1,37 +1,20 @@
 import asyncio
-from typing import TypeVar, Generic, Optional
 from cachetools import LRUCache
 from tools.utils import get_logger
+from ._cache_base import _CacheBase
+from ._types import KeyT, ValueT
+
 
 __all__ = ["LRUCacheService"]
 
-K = TypeVar("K")
-V = TypeVar("V")
 
-
-class LRUCacheService(Generic[K, V]):
+class LRUCacheService(_CacheBase[KeyT, ValueT]):
 
     def __init__(self, maxsize: int = 250) -> None:
         self._cache = LRUCache(maxsize=maxsize)
+        super().__init__(self._cache)
         self._logger = get_logger(__name__)
         asyncio.create_task(self.clear())
-
-    def add_item(self, key: K, value: V) -> bool:
-        if key not in self._cache:
-            self._cache[key] = value
-            return True
-        return False
-
-    def remove_item(self, key: K) -> None:
-        if key in self._cache:
-            del self._cache[key]
-        else:
-            raise KeyError(f"Key {key} not found in cache")
-
-    def get_item(self, key: K) -> Optional[V]:
-        if key in self._cache:
-            return self._cache[key]
-        return None
 
     async def clear(self) -> None:
         while True:
