479d69fad261b163d83f941947c65419b58e9fb8
Pedro <pedroh.fsouzars@gmail.com>
2025-01-30 18:37:06 -0300

    refact: update farm title to include 'Farmer' suffix when farmer is present

--- usecases/farm_usecases/farm_usecase.py
+++ usecases/farm_usecases/farm_usecase.py
@@ -32,9 +32,7 @@ class FarmUseCase:
                 )
             farm_entity = await self.farm_cache_service.create_farm(self.ctx.author.id)
 
-        farm_title = (
-            f"🚜 {farm_entity.farm_title}\n🧑‍🌾 Farmer: {farm_entity.farmer if farm_entity.farmer else 'No farmer'}"
-        )
+        farm_title = f"🚜 {farm_entity.farm_title}\n🧑‍🌾 Farmer: {farm_entity.farmer + ' Farmer' if farm_entity.farmer else 'No farmer'}"
         farm_chickens = "\n\n".join(
             [
                 f"{chicken.emoji} - **{chicken.rarity} {chicken.name}**"
