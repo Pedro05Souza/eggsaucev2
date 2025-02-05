d4d2de4f2e708632cdd9ced05e1e65d84231a2fe
Pedro <pedroh.fsouzars@gmail.com>
2025-02-05 19:46:58 -0300

    feat: bulk_update_farm method

--- repositories/protocols/farm_repository_protocol.py
+++ repositories/protocols/farm_repository_protocol.py
@@ -1,5 +1,6 @@
 from typing import Protocol, Optional
 from entities import FarmEntity, ChickenEntity
+from models import Farm
 
 __all__ = ["FarmRepositoryProtocol"]
 
@@ -10,5 +11,7 @@ class FarmRepositoryProtocol(Protocol):
     async def create_farm(self, discord_user_id: int) -> FarmEntity: ...
 
     async def update_farm(self, farm_entity: FarmEntity) -> FarmEntity: ...
+
+    async def bulk_update_farm(self, farm_entities: list[Farm]) -> None: ...
 
     async def upsert_farm_chicken(self, farm_id: str, chicken: ChickenEntity) -> None: ...
