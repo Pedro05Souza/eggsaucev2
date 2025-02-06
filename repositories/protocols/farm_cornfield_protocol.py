6c05d27e5693ef1d09c2fb92ac558966de6f3314
Leonardo <leonardoms.2010@hotmail.com>
2025-02-06 10:07:56 -0300

    feat: development of the farm_cornfield_protocol

---
+++ repositories/protocols/farm_cornfield_protocol.py
@@ -0,0 +1,16 @@
+from typing import Protocol, Optional
+from entities.farm_entities.farm_cornfield_entity import FarmCornfieldEntity
+from entities.farm_entities.farm_player_entity import FarmEntity
+
+
+__all__ = ["FarmCornfieldProtocol"]
+
+
+class FarmCornfieldProtocol(Protocol):
+    async def get_cornfield_by_user_discord_id(self, discord_user_id: int) -> Optional[FarmCornfieldEntity]: ...
+
+    async def create_cornfield(self, discord_user_id: int) -> FarmCornfieldEntity: ...
+
+    async def update_cornfield(self, farm_cornfield_entity: FarmCornfieldEntity) -> FarmCornfieldEntity: ...
+
+    async def upsert_farm(self, farm: FarmEntity) -> None: ...
