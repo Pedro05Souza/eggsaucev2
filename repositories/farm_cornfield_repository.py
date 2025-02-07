767a3176562fa7e0a593208ebe8ba13ec6482dff
Leonardo <leonardoms.2010@hotmail.com>
2025-02-07 09:28:17 -0300

    refact: farm_cornfield_repository

--- repositories/farm_cornfield_repository.py
+++ repositories/farm_cornfield_repository.py
@@ -12,7 +12,7 @@ class CornfieldRepository:
     async def get_cornfield_by_user_discord_id(self, discord_user_id: int) -> Optional[FarmCornfieldEntity]:
         cornfield = (
             await FarmCornfield.filter(farm__player__discord_user_id=discord_user_id)
-            .select_related("farm", "farm__player")
+            .select_related("farm")
             .first()
         )
         if not cornfield:
@@ -20,8 +20,7 @@ class CornfieldRepository:
         return await farm_cornfield_model_to_entity(cornfield)
 
     async def create_cornfield(self, discord_user_id: int) -> FarmCornfieldEntity:
-        player = await Player.get(discord_user_id=discord_user_id)
-        farm = await Farm.get(player=player)
+        farm = await Farm.get(player__discord_user_id=discord_user_id)
         corns = await FarmCornfield.create(farm=farm)
         return await farm_cornfield_model_to_entity(corns)
 
@@ -37,17 +36,3 @@ class CornfieldRepository:
         )
 
         return farm_cornfield_entity
-
-    async def upsert_farm(self, farm: FarmEntity) -> None:
-        await Farm.update_or_create(
-            id=farm.id,
-            defaults={
-                "player_id": farm.player_id,
-                "discord_user_id": farm.discord_user_id,
-                "farm_title": farm.farm_title,
-                "farmer": farm.farmer,
-                "next_drop_time": farm.next_drop_time,
-                "remaining_rolls": farm.remaining_rolls,
-                "next_chicken_roll_time": farm.next_chicken_roll_time,
-            },
-        )
