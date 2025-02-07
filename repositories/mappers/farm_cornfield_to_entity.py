bf8d3b10c58b2251d6579b81d5cc90133051d3b1
Leonardo <leonardoms.2010@hotmail.com>
2025-02-07 10:30:41 -0300

    refact: some changes on the name of atributtes of FarmCornfieldEntity

--- repositories/mappers/farm_cornfield_to_entity.py
+++ repositories/mappers/farm_cornfield_to_entity.py
@@ -7,7 +7,7 @@ __all__ = ["farm_cornfield_model_to_entity"]
 async def farm_cornfield_model_to_entity(farm_cornfield: FarmCornfield) -> FarmCornfieldEntity:
 
     return FarmCornfieldEntity(
-        farm_cornfield_id=str(farm_cornfield.id),
+        id=str(farm_cornfield.id),
         farm_id=farm_cornfield.farm_id,
         cornfield_name=farm_cornfield.cornfield_name,
         current_corn=farm_cornfield.current_corn,
