99962d50b7e5b97bfdc3a9699420084acbb22242
Pedro <pedroh.fsouzars@gmail.com>
2025-02-09 19:40:17 -0300

    refact: make last_bought_title mandatory in player_model_to_entity

--- repositories/mappers/player_to_entity.py
+++ repositories/mappers/player_to_entity.py
@@ -4,13 +4,15 @@ from tools.constants import AMOUNT_PER_BANK_UPGRADE
 
 __all__ = ["player_model_to_entity"]
 
+
 def player_model_to_entity(player: Player) -> PlayerEntity:
-    bank_player: BankPlayer = player.bank_player # type: ignore
+    bank_player: BankPlayer = player.bank_player  # type: ignore
+
     return PlayerEntity(
         id=str(player.id),
         discord_user_id=player.discord_user_id,
         balance=player.balance,
-        last_bought_title=player.last_bought_title.value if player.last_bought_title else None,
+        last_bought_title=player.last_bought_title.value,
         next_salary_time=player.next_salary_time,
         bank_balance=bank_player.balance,
         bank_capacity=bank_player.upgrade_level * AMOUNT_PER_BANK_UPGRADE,
