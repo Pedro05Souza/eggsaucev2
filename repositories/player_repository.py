c09ff094a4e1349d75df0c5f40f85e19959de96d
Pedro <pedroh.fsouzars@gmail.com>
2025-02-12 20:00:14 -0300

    feat: get_or_create

--- repositories/player_repository.py
+++ repositories/player_repository.py
@@ -10,6 +10,7 @@ __all__ = ["PlayerRepository"]
 class PlayerRepository:
 
     async def get_player_by_discord_id(self, discord_user_id: int) -> Optional[PlayerEntity]:
+        print("get_player_by_discord_id")
         database_player = await Player.get_or_none(discord_user_id=discord_user_id).select_related("bank_player")
 
         if not database_player:
@@ -17,11 +18,16 @@ class PlayerRepository:
 
         return player_model_to_entity(database_player)
 
-    async def create_player(self, discord_user_id: int, next_salary_time: datetime) -> PlayerEntity:
-        player = await Player.create(discord_user_id=discord_user_id, next_salary_time=next_salary_time)
-        await self._create_bank_player(player)
-        player = await Player.get(id=player.id).select_related("bank_player")
+    async def get_or_create(self, discord_user_id: int, next_salary_time: datetime) -> PlayerEntity:
+        player, creation_flag = await Player.get_or_create(
+            discord_user_id=discord_user_id,
+            defaults={"next_salary_time": next_salary_time, "discord_user_id": discord_user_id},
+        )
 
+        if creation_flag:
+            await self._create_bank_player(player)
+
+        player = await Player.get(id=player.id).select_related("bank_player")
         return player_model_to_entity(player)
 
     async def _create_bank_player(self, player: Player) -> BankPlayer:
@@ -40,4 +46,5 @@ class PlayerRepository:
         return player
 
     async def bulk_update_players(self, players: list[Player]) -> None:
+
         await Player.bulk_update(players, ["balance", "last_bought_title", "next_salary_time"])
