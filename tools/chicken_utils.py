2c764c9715edd8dbea1cb6054e8e1d3214076c68
Pedro <pedroh.fsouzars@gmail.com>
2025-02-13 19:27:31 -0300

    refact: improve formatting in calculate_corn_limit function

--- tools/chicken_utils.py
+++ tools/chicken_utils.py
@@ -53,8 +53,9 @@ def _generate_chicken_quality(chicken_rarity: str) -> float:
 def calculate_plot_production(number_of_plots: int) -> int:
     return DELTA_PLOT_PRODUCTION * number_of_plots
 
+
 def calculate_corn_limit(corn_limit_upgrades: int) -> int:
-    return DELTA_CORN_LIMIT * (corn_limit_upgrades ** 2)
+    return DELTA_CORN_LIMIT * (corn_limit_upgrades**2)
 
 
 async def generated_chicken_to_chicken_entity(
