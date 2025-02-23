198f0e91848c0b21918c911fa697e6e7d2678728
Pedro <pedroh.fsouzars@gmail.com>
2025-02-22 23:20:48 -0300

    refact: removed player cache

--- tools/globals.py
+++ tools/globals.py
@@ -1,12 +1,10 @@
-from repositories import PlayerRepository, BotConfigRepository, FarmRepository
-from .services import PlayerCacheService, BotConfigCacheService, FarmCacheService
+from repositories import BotConfigRepository, FarmRepository
+from .services import BotConfigCacheService, FarmCacheService
 
 __all__ = [
-    "GlobalPlayerCache",
     "GlobalBotConfigCache",
     "GlobalFarmCache",
 ]
 
-GlobalPlayerCache = PlayerCacheService(False, PlayerRepository())
 GlobalBotConfigCache = BotConfigCacheService(False, BotConfigRepository(), expiration_time=3600)
 GlobalFarmCache = FarmCacheService(True, FarmRepository())
