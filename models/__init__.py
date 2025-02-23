05d6a4d891ef6fe0367644a2767c0584d30170bb
Pedro <pedroh.fsouzars@gmail.com>
2025-02-23 01:54:10 -0300

    refact: remove farm_offers model and associated migration

--- models/__init__.py
+++ models/__init__.py
@@ -3,7 +3,6 @@ from .bot_config import *
 from .chicken import *
 from .cornfield import *
 from .farm import *
-from .farm_offers import *
 from .ranked_player import *
 from .player import *
 from .allowed_channels import *
