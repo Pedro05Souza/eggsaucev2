b4118e0abc32236b1fc83a5c7bbaab814085cffd
Pedro <pedroh.fsouzars@gmail.com>
2025-06-06 21:53:40 -0300

    refact: remove unecessary atomic decorator

--- tools/services/cache/farm_cache_service.py
+++ tools/services/cache/farm_cache_service.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 from asyncio import Lock
-from tortoise.transactions import atomic
+from entities.farm_entities.farm_player_entity import FarmEntity
 from repositories import FarmRepositoryProtocol
 from tools.reverse_mapping import farm_entity_to_model
 from ._entity_cache import EntityCacheService
@@ -39,11 +39,11 @@ class FarmCacheService(EntityCacheService["FarmEntity"]):
 
                 self.add(key, farm_entity)
             return farm_entity
+        
 
-    @atomic()
     async def _save_expired_or_removed_items(self):
         items_to_update = await self._get_expired_or_removed_items()
-
+        
         if len(items_to_update) == 0:
             return
 
